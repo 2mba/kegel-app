e488f8ddc26320292291d3eefa1f8aea8bdede99
Santaev Pavel <santaevpa@mail.skblab.ru>
2020-08-01 01:20:01 +0700

    remove navbar

--- app/src/main/java/org/tumba/kegel_app/MainActivity.kt
+++ app/src/main/java/org/tumba/kegel_app/MainActivity.kt
@@ -10,26 +10,8 @@ import org.tumba.kegel_app.utils.show
 
 class MainActivity : AppCompatActivity() {
 
-    private val navBarScreens = listOf(
-        R.id.screen_home,
-        R.id.screen_notifications
-    )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initNavigation()
-    }
-
-    private fun initNavigation() {
-        val navController = findNavController(R.id.navFragment)
-        navView.setupWithNavController(navController)
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            if (destination.id in navBarScreens) {
-                navView.show()
-            } else {
-                navView.gone()
-            }
-        }
     }
 }
