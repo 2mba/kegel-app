57bc6e153818f6bb5a83204d3b6723cacec49935
Pavel Santaev <santaevp@gmail.com>
2021-05-19 00:49:37 +0300

    add sound packs

--- app/src/main/java/org/tumba/kegel_app/domain/ExerciseEffectsHandler.kt
+++ app/src/main/java/org/tumba/kegel_app/domain/ExerciseEffectsHandler.kt
@@ -1,5 +1,6 @@
 package org.tumba.kegel_app.domain
 
+import androidx.annotation.RawRes
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -8,6 +9,8 @@ import kotlinx.coroutines.launch
 import org.tumba.kegel_app.core.system.SoundManager
 import org.tumba.kegel_app.core.system.VibrationManager
 import org.tumba.kegel_app.repository.ExerciseSettingsRepository
+import org.tumba.kegel_app.sound.SoundPack
+import org.tumba.kegel_app.sound.SoundPackManager
 import javax.inject.Inject
 
 interface ExerciseEffectsHandler {
@@ -18,32 +21,49 @@ interface ExerciseEffectsHandler {
 class ExerciseEffectsHandlerImpl @Inject constructor(
     private val exerciseSettingsRepository: ExerciseSettingsRepository,
     private val vibrationManager: VibrationManager,
-    private val soundManager: SoundManager
+    private val soundManager: SoundManager,
+    soundPackManager: SoundPackManager
 ) : ExerciseEffectsHandler {
 
     private val coroutineScope = CoroutineScope(GlobalScope.coroutineContext + Dispatchers.Main)
     private var soundVolume = 0f
+    private var soundPack: SoundPack? = null
+    private val allSoundPacks = soundPackManager.getAllPacks()
 
     override fun onStartExercise(state: Flow<ExerciseState>) {
         soundManager.build()
+        observeSoundVolume()
+        observeSoundPack()
+        coroutineScope.launch { handleExerciseKindChanges(state) }
+        coroutineScope.launch { handleExerciseFinish(state) }
+    }
+
+    private fun observeSoundVolume() {
         coroutineScope.launch {
             exerciseSettingsRepository.soundVolume
                 .asFlow()
                 .flowOn(Dispatchers.Default)
                 .collect { soundVolume = it }
         }
-        coroutineScope.launch { handleExerciseKindChanges(state) }
-        coroutineScope.launch { handleExerciseFinish(state) }
+    }
+
+    private fun observeSoundPack() {
+        coroutineScope.launch {
+            exerciseSettingsRepository.soundPack
+                .asFlow()
+                .flowOn(Dispatchers.Default)
+                .collect { soundPack = allSoundPacks.getOrNull(it) }
+        }
     }
 
     private suspend fun handleExerciseKindChanges(state: Flow<ExerciseState>) {
-        state.filterIsInstance<ExerciseState.SingleExercise>()
-            .filter { it !is ExerciseState.Pause && it !is ExerciseState.Preparation }
+        state//.filterIsInstance<ExerciseState.SingleExercise>()
+            // .filter { it !is ExerciseState.Pause && it !is ExerciseState.Preparation }
             .distinctUntilChanged { exercise1, exercise2 -> exercise1.javaClass == exercise2.javaClass }
             .flowOn(Dispatchers.Default)
-            .collect {
-                vibrate()
-                playSound()
+            .collect { exerciseState ->
+                vibrate(exerciseState)
+                playSound(exerciseState)
             }
     }
 
@@ -52,15 +72,34 @@ class ExerciseEffectsHandlerImpl @Inject constructor(
             .collect { soundManager.release() }
     }
 
-    private fun vibrate() {
+    private fun vibrate(state: ExerciseState) {
+        if (state is ExerciseState.Pause || state is ExerciseState.Preparation) {
+            return
+        }
         if (exerciseSettingsRepository.isVibrationEnabled.value) {
             vibrationManager.vibrate(VibrationManager.Strength.Medium)
         }
     }
 
-    private fun playSound() {
-        if (exerciseSettingsRepository.isSoundEnabled.value) {
-            soundManager.play(exerciseSettingsRepository.soundVolume.value)
+    private fun playSound(state: ExerciseState) {
+        if (state is ExerciseState.Pause || state is ExerciseState.Preparation) {
+            return
+        }
+        val soundId = getSoundId(state)
+        if (exerciseSettingsRepository.isSoundEnabled.value && soundId != null) {
+            soundManager.play(exerciseSettingsRepository.soundVolume.value, soundId)
+        }
+    }
+
+    @RawRes
+    private fun getSoundId(state: ExerciseState): Int? {
+        return soundPack?.let { soundPack ->
+            when (state) {
+                is ExerciseState.Preparation -> soundPack.preparationSoundId
+                is ExerciseState.Holding -> soundPack.holdSoundId
+                is ExerciseState.Relax -> soundPack.relaxSoundId
+                else -> null
+            }
         }
     }
 }
